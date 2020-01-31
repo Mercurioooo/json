2daba15ac3b74dbd280f85695f91df74a11e78fe
ljn <1192439854@qq.com>
2020-01-31 11:33:36 +0800

    代码

---
+++ JsonProject/src/Test/Address.java
@@ -0,0 +1,26 @@
+package Test;
+
+public class Address {
+	String homeAddress;
+	String schoolAddress;
+	public Address() {
+	}
+	public Address(String homeAddress, String schoolAddress) {
+		super();
+		this.homeAddress = homeAddress;
+		this.schoolAddress = schoolAddress;
+	}
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public String getSchoolAddress() {
+		return schoolAddress;
+	}
+	public void setSchoolAddress(String schoolAddress) {
+		this.schoolAddress = schoolAddress;
+	}
+	
+}
